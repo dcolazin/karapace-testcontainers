63f903ba3610f79eb76533477101d0d325970690
dcolazin <dcolazin@gmail.com>
2025-11-16 11:03:46 +0100

    initial commit

---
+++ src/main/java/org/vetronauta/testcontainers/karapace/KarapaceContainer.java
@@ -0,0 +1,135 @@
+package org.vetronauta.testcontainers.karapace;
+
+import lombok.NonNull;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KarapaceContainer extends GenericContainer<KarapaceContainer> {
+
+    public static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName
+        .parse("aiven-open/karapace")
+        .withRegistry("ghcr.io");
+    public static final int ORIGINAL_EXPOSED_PORT = 8081;
+
+    private static final Map<String, String> ENV_MAP;
+
+    private final boolean shouldTearDownKafka;
+    private final KafkaContainer kafkaContainer;
+
+    static {
+        //TODO remove defaults
+        Map<String, String> envMap = new HashMap<>();
+        envMap.put("KARAPACE_KARAPACE_REGISTRY", "true");
+        envMap.put("KARAPACE_ADVERTISED_HOSTNAME", "karapace-schema-registry");
+        envMap.put("KARAPACE_ADVERTISED_PROTOCOL", "http");
+        envMap.put("KARAPACE_BOOTSTRAP_URI", "kafka:9093");
+        envMap.put("KARAPACE_PORT", "8081");
+        envMap.put("KARAPACE_HOST", "0.0.0.0");
+        envMap.put("KARAPACE_CLIENT_ID", "karapace-schema-registry-0");
+        envMap.put("KARAPACE_GROUP_ID", "karapace-schema-registry");
+        envMap.put("KARAPACE_MASTER_ELECTION_STRATEGY", "highest");
+        envMap.put("KARAPACE_MASTER_ELIGIBILITY", "true");
+        envMap.put("KARAPACE_TOPIC_NAME", "_schemas");
+        envMap.put("KARAPACE_LOG_LEVEL", "INFO");
+        envMap.put("KARAPACE_COMPATIBILITY", "FULL");
+        envMap.put("KARAPACE_STATSD_HOST", "statsd-exporter");
+        envMap.put("KARAPACE_STATSD_PORT", "8125");
+        envMap.put("KARAPACE_KAFKA_SCHEMA_READER_STRICT_MODE", "false");
+        envMap.put("KARAPACE_KAFKA_RETRIABLE_ERRORS_SILENCED", "true");
+        envMap.put("KARAPACE_TAGS__APP", "karapace-schema-registry");
+        ENV_MAP = Collections.unmodifiableMap(envMap);
+    }
+
+    private KarapaceContainer(Builder builder) {
+        super(builder.karapaceImageName);
+        if (builder.assertCompatible) {
+            builder.karapaceImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
+        }
+        if (builder.kafkaContainer != null) {
+            this.kafkaContainer = builder.kafkaContainer;
+            this.shouldTearDownKafka = false;
+        } else {
+            //3.9.0 is not the best to use https://issues.apache.org/jira/browse/KAFKA-18281
+            this.kafkaContainer = new KafkaContainer("apache/kafka:3.9.1")
+                .withNetworkAliases("kafka")
+                .withNetwork(Network.builder().driver("bridge").build());
+            this.shouldTearDownKafka = true;
+        }
+        addExposedPort(ORIGINAL_EXPOSED_PORT);
+        dependsOn(this.kafkaContainer);
+        setNetwork(this.kafkaContainer.getNetwork());
+        setCommand("python3 -m karapace");
+        setWaitStrategy(Wait.forHttp("/_health"));
+        ENV_MAP.forEach(this::addEnv);
+    }
+
+    @Override
+    public void start() {
+        if (!kafkaContainer.isRunning()) {
+            kafkaContainer.start();
+        }
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        if (shouldTearDownKafka) {
+            kafkaContainer.stop();
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    //TODO use @With?
+    public static class Builder {
+        DockerImageName karapaceImageName = DEFAULT_IMAGE_NAME;
+        String karapaceTag;
+        KafkaContainer kafkaContainer;
+        boolean assertCompatible = true;
+
+        public Builder karapaceImageName(@NonNull String karapaceImageName) {
+            this.karapaceImageName = DockerImageName.parse(karapaceImageName);
+            return this;
+        }
+
+        public Builder karapaceImageName(@NonNull DockerImageName karapaceImageName) {
+            this.karapaceImageName = karapaceImageName;
+            return this;
+        }
+
+        public Builder karapaceTag(String karapaceTag) {
+            this.karapaceTag = karapaceTag;
+            return this;
+        }
+
+        public Builder kafkaContainer(KafkaContainer kafkaContainer) {
+            this.kafkaContainer = kafkaContainer;
+            return this;
+        }
+
+        public Builder assertCompatible(boolean assertCompatible) {
+            this.assertCompatible = assertCompatible;
+            return this;
+        }
+
+        public KarapaceContainer build() {
+            if (karapaceTag != null) {
+                //TODO make sure that DEFAULT_IMAGE_NAME is immutable
+                karapaceImageName = karapaceImageName.withTag(karapaceTag);
+            }
+            return new KarapaceContainer(this);
+        }
+
+    }
+
+}
---
+++ src/test/java/org/vetronauta/testcontainers/karapace/KarapaceContainerTest.java
@@ -0,0 +1,37 @@
+package org.vetronauta.testcontainers.karapace;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KarapaceContainerTest {
+
+    @Test
+    void startStopTest() throws URISyntaxException, IOException, InterruptedException {
+        try (KarapaceContainer karapaceContainer = KarapaceContainer.builder().build();
+             HttpClient client = HttpClient.newHttpClient()) {
+
+            karapaceContainer.start();
+
+            int port = karapaceContainer.getMappedPort(KarapaceContainer.ORIGINAL_EXPOSED_PORT);
+            String baseUri = String.format("http://%s:%s", karapaceContainer.getHost(), port);
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(baseUri + "/subjects"))
+                .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Assert.assertEquals(response.statusCode(), 200);
+            Assert.assertEquals(response.body(), "[]");
+
+            karapaceContainer.stop();
+        }
+
+    }
+
+}
